22fedd5af0d3695208e579784ebe4cdaa8fd21c4
okansavas <okansav@gmail.com>
2024-03-01 21:58:57 +0100

    Test5 olusturuldu

---
+++ src/SiparisListesiKontrolü_US5/siparislistesikontrolu.java
@@ -0,0 +1,11 @@
+package SiparisListesiKontrolü_US5;
+
+import Utility.BaseDriver;
+import org.junit.Test;
+public class siparislistesikontrolu extends BaseDriver {
+
+   @Test
+    public void Test1() {
+
+    }
+}
