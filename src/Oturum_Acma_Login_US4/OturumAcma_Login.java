4b30fb2a4c30a377c4e41fcde514447a58e2f230
turuhsen <tubaaldemir1013@gmail.com>
2024-03-01 22:04:20 +0100

    Test4 oldu

---
+++ src/Oturum_Acma_Login_US4/OturumAcma_Login.java
@@ -0,0 +1,14 @@
+package Oturum_Acma_Login_US4;
+
+import Utility.BaseDriver;
+import com.google.common.annotations.VisibleForTesting;
+import org.junit.Test;
+
+
+public class OturumAcma_Login extends BaseDriver {
+   @Test
+    public void Test4(){
+
+   }
+
+}
