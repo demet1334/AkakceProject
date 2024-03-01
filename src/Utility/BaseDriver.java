4d44d43c51145bba3b76b0374f38b60058673f7d
sammycaglar <caglarsemih1@outlook.com>
2024-03-01 23:57:49 +0300

    TC_0601 oluşturuldu

--- src/Utility/BaseDriver.java
+++ src/Utility/BaseDriver.java
@@ -12,17 +12,24 @@ public class BaseDriver {
     public static WebDriver driver;
     public static WebDriverWait wait;
 
-    static{ //bunun sarti extends olmasi ve basta yer almasi
-        driver=new ChromeDriver();
+    static {// public static void DriverBaslat() varmış gibi davranıp extend
+        // edilen yerde ilk başta buy method direkt olarak çalışır
 
-        //driver.manage().window().maximize(); // Ekranı max yapıyor.
+        // uyarıları gizleme
+        Logger logger = Logger.getLogger(""); // output yapılan logları al.
+        logger.setLevel(Level.SEVERE);             // sadece ERROR ları göster
+
+        driver = new ChromeDriver();
+        driver.manage().window().maximize(); // Ekranı max yapıyor
         driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20)); // 20 sn mühlet: sayfayı yükleme mühlet
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20)); // 20 sn mühlet: elementi bulma mühleti
-        wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30)); // 20 sn mühlet: elementi bulma mühleti
+        wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+
     }
 
-    public static void BekleKapat(){
-        MyFunc.Bekle(3);
+    public static void BekleKapat() {
+        MyFunc.Bekle(2);
         driver.quit();
     }
+
 }
