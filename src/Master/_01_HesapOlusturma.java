291ab9bda031315e96254ccbd4d5d75767496711
Admin <sidagulov@gmail.com>
2024-03-02 02:57:34 +0200

    serik3

--- src/Master/_01_HesapOlusturma.java
+++ src/Master/_01_HesapOlusturma.java
@@ -1,7 +1,19 @@
 package Master;
 
 import Utility.BaseDriver;
+import com.google.common.annotations.VisibleForTesting;
+import org.junit.Test;
 
 public class _01_HesapOlusturma extends BaseDriver {
 
+    @Test
+    public void Test1(){
+
+        System.out.println("Demet calisti simdi");
+
+
+
+        BekleKapat();
+
+    }
 }
