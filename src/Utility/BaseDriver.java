993ed029f0d74ddb406b42f0631cf6265cad4e99
burak <burak_demi1@web.de>
2024-03-04 00:56:26 +0100

    OturumKapat Testi basarili bir sekilde tamamlanmisdir

--- src/Utility/BaseDriver.java
+++ src/Utility/BaseDriver.java
@@ -15,7 +15,7 @@ public class BaseDriver {
     static{ //bunun sarti extends olmasi ve basta yer almasi
         driver=new ChromeDriver();
 
-        //driver.manage().window().maximize(); // Ekranı max yapıyor.
+        driver.manage().window().maximize(); // Ekranı max yapıyor.
         driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20)); // 20 sn mühlet: sayfayı yükleme mühlet
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20)); // 20 sn mühlet: elementi bulma mühleti
         wait = new WebDriverWait(driver, Duration.ofSeconds(20));
